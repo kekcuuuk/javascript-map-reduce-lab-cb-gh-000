1a543ec0aa6a0cb6b9a0571d5f0d8c4344f1d194
Igor Morgunov <kekcuuuk@gmail.com>
2017-11-15 19:44:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9001,4 +9001,4 @@ const issues = [
   }
 ];
 
-//const issuesWithUpdatedApiUrl = issues.map(issue => {'url' in issue ? issue.url.replace(/api\.github\.com/, 'api-v2.github.com') : null;});
+let issuesWithUpdatedApiUrl = issues.map(issue => {'url' in issue ? issue.url.replace(/api\.github\.com/, 'api-v2.github.com') : null;});
